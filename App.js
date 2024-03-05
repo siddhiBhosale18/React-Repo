e71e6bdb7f2e729575438ee45781e1f0873dffff
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-03-05 10:38:14 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+// const heading = React.createElement("h1", { id: "Heading" }, "Hello World !....React");
+// // (elementName,{Atributes},"innerHtml/text")
+// // React.createElement  creates a react Object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+    "div", { id: "parent" },
+    React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am H1 Tag"), React.createElement("h2", {}, "I am H2 Tag")]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
