7aa2228dfc1b1b5bbc37869d174ba4bdf63c5755
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-03-19 21:47:41 +0530

    commit

--- components/Body.js
+++ components/Body.js
@@ -1,13 +1,25 @@
-import resList from "../utils/mockData"
+import resList from "../utils/mockData";
 import RestaurentCard from "./RestaurentCard";
+import { useState } from "react";
 
 const Body=()=>{
+    const [listOfRestaurent,setListOfRestaurent]=useState(resList);
+
     return(
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" 
+                onClick={()=>
+                    {
+                        const filteredList=listOfRestaurent.filter((res)=>res.info.avgRating>4);
+                        setListOfRestaurent(filteredList);
+                    }
+                }
+                >Tap To Filter Restaurent</button>
+            </div>
             <div className="res-container">
                 {
-                    resList.map((restaurent)=><RestaurentCard key={restaurent.info.id } resData={restaurent}  />)
+                    listOfRestaurent.map((restaurent)=><RestaurentCard key={restaurent.info.id } resData={restaurent}  />)
                 }
             </div>
         </div>
