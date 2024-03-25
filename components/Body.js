a9438a153301d0246b2ac51840c4d018da24879e
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-03-25 22:20:18 +0530

    commit

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,32 @@
 import resList from "../utils/mockData";
 import RestaurentCard from "./RestaurentCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body=()=>{
     const [listOfRestaurent,setListOfRestaurent]=useState(resList);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData=async()=>{
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json();
+        console.log(json);
+    }
+
+    // const fetchData = async () => {
+    //     try {
+    //         const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    //         if (!response.ok) {
+    //             throw new Error('Failed to fetch data');
+    //         }
+    //         const json = await response.json();
+    //         console.log(json);
+    //     } catch (error) {
+    //         console.error('Error fetching data:', error);
+    //     }
+    // }
 
     return(
         <div className="body">
