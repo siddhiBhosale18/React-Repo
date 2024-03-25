8e9ae9a95abea6ca73a0a154bd7fd85f11607f67
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-03-26 00:05:00 +0530

    commit

--- components/Body.js
+++ components/Body.js
@@ -9,10 +9,32 @@ const Body=()=>{
         fetchData();
     },[]);
 
-    const fetchData=async()=>{
-        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-        const json = await data.json();
-        console.log(json);
+    // const fetchData=async()=>{
+    //     const data=await fetch("https://api.allorigins.win/get?"+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    //     // console.log(data);
+    //     const json = await data.json();
+    //     console.log(json);
+    // }
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("https://api.allorigins.win/get?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            const json = await response.json();
+    
+            // Check if json is empty
+            if (!json) {
+                throw new Error('Empty JSON response');
+            }
+    
+            console.log(json);
+        } catch (error) {
+            console.error('Error fetching or parsing data:', error);
+        }
     }
 
     // const fetchData = async () => {
